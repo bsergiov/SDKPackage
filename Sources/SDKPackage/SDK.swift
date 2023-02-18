99f3b7cf22eb374a7439f760cf7f01a834a52529
Bsergio <bsergiovi@gmail.com>
2023-02-19 00:40:34 +0200

    v 1.0.1

--- Sources/SDKPackage/SDK.swift
+++ Sources/SDKPackage/SDK.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public struct SDK {
      public  static func doSomeWork() {
-        print("This some work from App target")
+        print("This some work from package")
     }
 }
