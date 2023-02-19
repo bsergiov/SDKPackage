495f870449384d76b5ac1f9972f99236260b741f
Bsergio <bsergiovi@gmail.com>
2023-02-19 02:08:25 +0200

    v2.0.0

---
+++ Sources/SDKPackage.xcframework/ios-arm64_x86_64-simulator/FramworkProject.framework/Headers/FramworkProject.h
@@ -0,0 +1,18 @@
+//
+//  FramworkProject.h
+//  FramworkProject
+//
+//  Created by Сергей Бровко on 18.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FramworkProject.
+FOUNDATION_EXPORT double FramworkProjectVersionNumber;
+
+//! Project version string for FramworkProject.
+FOUNDATION_EXPORT const unsigned char FramworkProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FramworkProject/PublicHeader.h>
+
+
